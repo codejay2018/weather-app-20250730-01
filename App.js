58fafa5d6c932373dfe7526bd602640f8a5518e0
codejay2018 <codejay2018@gmail.com>
2025-07-31 18:20:04 +0900

    메인 페이지 레이아웃 구성

--- App.js
+++ App.js
@@ -8,9 +8,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.v1}></View>
-      <View style={styles.v2}></View>
-      <View style={styles.v3}></View>
+      <View style={styles.cityContainer}>
+        <Text style={styles.city}>Silim</Text>
+      </View>
+      <View style={styles.weatherContainer}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>7월 31일 (목) 오후 5:35</Text>
+          <Text style={styles.desc}>맑음</Text>
+        </View>
+        <View style={styles.tempContainer}>
+          <Text style={styles.temp}>24</Text>
+        </View>        
+      </View>
+           
       <StatusBar style="auto" />
     </View>
   );
@@ -18,15 +28,63 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container:{
-   flex:1
+   flex:1,
+   backgroundColor:"#ffe01a"
   },
-  v1:{
-    flex:1, backgroundColor:"red"
+  cityContainer:{
+    flex:1, 
+    // backgroundColor:"blue"
   },  
-  v2:{
-    flex:1, backgroundColor:"darkorange"
+  city:{
+    flex:1, 
+    // backgroundColor:"red", 
+    marginTop:50,
+    fontSize:40,
+    textAlign:"center",
+    fontWeight:"bold",
+    paddingTop:20
   },  
-  v3:{
-    flex:1, backgroundColor:"green"
+  weatherContainer:{
+    flex:3, 
+    // backgroundColor:"green"
+  },  
+  day:{
+    flex:0.2,
+    // backgroundColor:"red",
+    textAlign:"center",
+    fontWeight:"bold",
+    fontSize:30,
+    alignItems:"center",
+    justifyContent:"center",
+  },  
+  regDate:{
+    backgroundColor:"black",
+    color:"white",
+    paddingTop:10,
+    paddingBottom:15,
+    paddingLeft:20,
+    paddingRight:20,
+    borderRadius:20,
+    overflow:"hidden",
+    fontWeight:"bold",
   },
+
+  desc:{
+    flex:1.5,
+    fontSize:25,
+    fontWeight:"bold",
+    marginTop:20,
+  },
+  tempContainer:{
+    flex:0.5, 
+    // backgroundColor:"blue",
+    alignItems:"center",
+    justifyContent:"center",
+  },  
+  temp:{
+    // backgroundColor:"green",
+    fontSize:120,
+  },
+
+
 });
