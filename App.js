64609a4124d7596e971955382980edfe7e473093
codejay2018 <codejay2018@gmail.com>
2025-08-01 22:14:15 +0900

    날씨, 온도 적용, 더피파일 사용

--- App.js
+++ App.js
@@ -1,14 +1,426 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Button, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { 
+  ActivityIndicator, 
+  Dimensions, 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  View } from 'react-native';
 import * as Location from 'expo-location';
 import { GOOGLE_API_KEY, OPEN_WEATHER_API_KEY } from '@env';
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
-export default function App() {
+const dummyWeatherData = 
+{
+   "lat":33.44,
+   "lon":-94.04,
+   "timezone":"America/Chicago",
+   "timezone_offset":-18000,
+   "current":{
+      "dt":1684929490,
+      "sunrise":1684926645,
+      "sunset":1684977332,
+      "temp":292.55,
+      "feels_like":292.87,
+      "pressure":1014,
+      "humidity":89,
+      "dew_point":290.69,
+      "uvi":0.16,
+      "clouds":53,
+      "visibility":10000,
+      "wind_speed":3.13,
+      "wind_deg":93,
+      "wind_gust":6.71,
+      "weather":[
+         {
+            "id":803,
+            "main":"Clouds",
+            "description":"broken clouds",
+            "icon":"04d"
+         }
+      ]
+   },
+   "minutely":[
+      {
+         "dt":1684929540,
+         "precipitation":0
+      },
+      {
+         "dt":1684929540,
+         "precipitation":0
+      },
+      {
+         "dt":1684929540,
+         "precipitation":0
+      },
 
-  const [number, setNumber] = useState(0);
+   ],
+   "hourly":[
+      {
+         "dt":1684926000,
+         "temp":292.01,
+         "feels_like":292.33,
+         "pressure":1014,
+         "humidity":91,
+         "dew_point":290.51,
+         "uvi":0,
+         "clouds":54,
+         "visibility":10000,
+         "wind_speed":2.58,
+         "wind_deg":86,
+         "wind_gust":5.88,
+         "weather":[
+            {
+               "id":803,
+               "main":"Clouds",
+               "description":"broken clouds",
+               "icon":"04n"
+            }
+         ],
+         "pop":0.15
+      },
+      {
+         "dt":1684926000,
+         "temp":292.01,
+         "feels_like":292.33,
+         "pressure":1014,
+         "humidity":91,
+         "dew_point":290.51,
+         "uvi":0,
+         "clouds":54,
+         "visibility":10000,
+         "wind_speed":2.58,
+         "wind_deg":86,
+         "wind_gust":5.88,
+         "weather":[
+            {
+               "id":803,
+               "main":"Clouds",
+               "description":"broken clouds",
+               "icon":"04n"
+            }
+         ],
+         "pop":0.15
+      },
+   ],
+   "daily":[
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":29.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+      {
+         "dt":1684951200,
+         "sunrise":1684926645,
+         "sunset":1684977332,
+         "moonrise":1684941060,
+         "moonset":1684905480,
+         "moon_phase":0.16,
+         "summary":"Expect a day of partly cloudy with rain",
+         "temp":{
+            "day":299.03,
+            "min":290.69,
+            "max":300.35,
+            "night":291.45,
+            "eve":297.51,
+            "morn":292.55
+         },
+         "feels_like":{
+            "day":299.21,
+            "night":291.37,
+            "eve":297.86,
+            "morn":292.87
+         },
+         "pressure":1016,
+         "humidity":59,
+         "dew_point":290.48,
+         "wind_speed":3.98,
+         "wind_deg":76,
+         "wind_gust":8.92,
+         "weather":[
+            {
+               "id":500,
+               "main":"Rain",
+               "description":"light rain",
+               "icon":"10d"
+            }
+         ],
+         "clouds":92,
+         "pop":0.47,
+         "rain":0.15,
+         "uvi":9.23
+      },
+
+   ],
+    "alerts": [
+    {
+      "sender_name": "NWS Philadelphia - Mount Holly (New Jersey, Delaware, Southeastern Pennsylvania)",
+      "event": "Small Craft Advisory",
+      "start": 1684952747,
+      "end": 1684988747,
+      "description": "...SMALL CRAFT ADVISORY REMAINS IN EFFECT FROM 5 PM THIS\nAFTERNOON TO 3 AM EST FRIDAY...\n* WHAT...North winds 15 to 20 kt with gusts up to 25 kt and seas\n3 to 5 ft expected.\n* WHERE...Coastal waters from Little Egg Inlet to Great Egg\nInlet NJ out 20 nm, Coastal waters from Great Egg Inlet to\nCape May NJ out 20 nm and Coastal waters from Manasquan Inlet\nto Little Egg Inlet NJ out 20 nm.\n* WHEN...From 5 PM this afternoon to 3 AM EST Friday.\n* IMPACTS...Conditions will be hazardous to small craft.",
+      "tags": [
+
+      ]
+    },
+    {
+      "sender_name": "NWS Philadelphia - Mount Holly (New Jersey, Delaware, Southeastern Pennsylvania)",
+      "event": "Small Craft Advisory",
+      "start": 1684952747,
+      "end": 1684988747,
+      "description": "...SMALL CRAFT ADVISORY REMAINS IN EFFECT FROM 5 PM THIS\nAFTERNOON TO 3 AM EST FRIDAY...\n* WHAT...North winds 15 to 20 kt with gusts up to 25 kt and seas\n3 to 5 ft expected.\n* WHERE...Coastal waters from Little Egg Inlet to Great Egg\nInlet NJ out 20 nm, Coastal waters from Great Egg Inlet to\nCape May NJ out 20 nm and Coastal waters from Manasquan Inlet\nto Little Egg Inlet NJ out 20 nm.\n* WHEN...From 5 PM this afternoon to 3 AM EST Friday.\n* IMPACTS...Conditions will be hazardous to small craft.",
+      "tags": [
+
+      ]
+    },
+  ]
+};
+
+
+export default function App() {
 
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -16,9 +428,6 @@ export default function App() {
   const [cityName, setCityName] = useState("Silim");
   const [dailyWeather, setDailyWeather] = useState([]);
 
-  const loadWeaterData = async () => {
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
-  }
 
   const locationData = async () => {
     const {granted} = await Location.requestForegroundPermissionsAsync();
@@ -52,20 +461,32 @@ export default function App() {
       // 모바일에서만 동작한다면, 이코드를 사용하자.
       const address = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false});
       console.log("address : ", address);
-      setCityName(address[0].city);
+
+      if (address.length > 0 && address[0].city) {
+        setCityName(address[0].city);
+      } else {
+        setCityName("Unknown");
+      }
+
     }
   
-    const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={alerts}&appid=${OPEN_WEATHER_API_KEY}`;
-    const resWeather = await fetch(weatherUrl);
-    const dataWeather = await resWeather.json();
+    // 더미 데이터로 테스트위해 주석처리함.
+    // const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={alerts}&units=metric&lang=kr&appid=${OPEN_WEATHER_API_KEY}`;
+    // const resWeather = await fetch(weatherUrl);
+    // const dataWeather = await resWeather.json();
+    // console.log("dataWeather : ", dataWeather);
+    const dataWeather = dummyWeatherData;
+
     console.log("dataWeather : ", dataWeather);
-    
+    console.log("dataWeather.daily : ", dataWeather.daily);
+  
+    setDailyWeather(dataWeather.daily);
   }
 
   useEffect(()=>{
     
     locationData();
-    loadWeaterData();
+
 
   }, []);
 
@@ -82,40 +503,28 @@ export default function App() {
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather} >
-        <View style={styles.weatherInner}>
-          <View style={styles.day}>
-            <Text style={styles.desc}>맑음</Text>
-          </View>
-          <View style={styles.tempContainer}>
-            <Text style={styles.temp}>24</Text>
-          </View>        
-        </View>
-        <View style={styles.weatherInner}>
-          <View style={styles.day}>
-            <Text style={styles.desc}>맑음</Text>
-          </View>
-          <View style={styles.tempContainer}>
-            <Text style={styles.temp}>24</Text>
-          </View>        
-        </View>
-        <View style={styles.weatherInner}>
-          <View style={styles.day}>
-
-            <Text style={styles.desc}>맑음</Text>
-          </View>
-          <View style={styles.tempContainer}>
-            <Text style={styles.temp}>24</Text>
-          </View>        
-        </View>
-        <View style={styles.weatherInner}>
-          <View style={styles.day}>
-            
-            <Text style={styles.desc}>맑음</Text>
-          </View>
-          <View style={styles.tempContainer}>
-            <Text style={styles.temp}>24</Text>
-          </View>        
-        </View>
+        {
+          dailyWeather.length === 0 ? (
+            <View style={styles.weatherInner}>
+              <ActivityIndicator/>
+            </View>
+          ) : (
+            dailyWeather.map((day, index)=>(
+              <View key={index} style={styles.weatherInner}>
+                <View style={styles.day}>
+                  <Text style={styles.desc}>{day.weather[0].description}</Text>
+                </View>
+                <View style={styles.tempContainer}>
+                  <Text style={styles.temp}>{
+                  parseFloat(day.temp.day).toFixed(0)
+                  }</Text>
+                  <Text style={styles.tempSymbal}>℃</Text>
+                </View>        
+              </View>
+            ))
+          )
+        }
+        
       </ScrollView>
 
            
@@ -168,7 +577,7 @@ const styles = StyleSheet.create({
     // backgroundColor:"green"
   },  
   day:{
-    flex:0.2,
+    flex:0.15,
     // backgroundColor:"red",
     textAlign:"center",
     fontWeight:"bold",
@@ -193,6 +602,12 @@ const styles = StyleSheet.create({
     // backgroundColor:"green",
     fontSize:120,
   },
+  tempSymbal:{
+    fontSize:50,
+    position:"absolute",
+    top:70,
+    right:100,
+  }
 
 
 });
